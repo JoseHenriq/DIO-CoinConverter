39c8ddb057a7d6878284ffcb9833d006756531dc
JoseHenriq <j.henriq@uol.com.br>
2021-11-03 18:27:24 -0300

    Commit 1 - Projeto utilizado no bootcamp

---
+++ app/src/main/java/br/com/dio/coinconverter/domain/di/DomainModule.kt
@@ -0,0 +1,25 @@
+package br.com.dio.coinconverter.domain.di
+
+import br.com.dio.coinconverter.domain.GetExchangeValueUseCase
+import br.com.dio.coinconverter.domain.ListExchangeUseCase
+import br.com.dio.coinconverter.domain.SaveExchangeUseCase
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+object DomainModule {
+
+    //--- Carrega os modulos a terem suas dependências injetadas
+    fun load() {
+        loadKoinModules(useCaseModules())
+    }
+
+    //--- Modulo useCaseModule
+    private fun useCaseModules(): Module {
+        return module {
+            factory { ListExchangeUseCase(get()) }
+            factory { SaveExchangeUseCase(get()) }
+            factory { GetExchangeValueUseCase(get()) }
+        }
+    }
+}
---
+++ app/src/main/java/br/com/dio/coinconverter/presentation/di/PresentationModule.kt
@@ -0,0 +1,26 @@
+package br.com.dio.coinconverter.presentation.di
+
+import br.com.dio.coinconverter.presentation.HistoryViewModel
+import br.com.dio.coinconverter.presentation.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+object PresentationModule {
+
+    //--- Carrega os modulos a terem suas dependências injetadas
+    fun load() {
+        //------------------------------------
+        loadKoinModules(viewModelModules())
+        //------------------------------------
+    }
+
+    //--- Modulo viewModelModule
+    private fun viewModelModules(): Module {
+        return module {
+            viewModel { HistoryViewModel(get()) }
+            viewModel { MainViewModel(get(), get()) }
+        }
+    }
+}
